91b5899a41449a0fe10d2db885ce9f29049608da
David Lukac <david@mogdesign.eu>
2013-04-29 22:23:31 +0200

    files generated by the script - 'rails new ./microblog/'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'd16a7b8e9cdded75d02e932036c1e258e5348425d6a8cf741baaa4dd81b6968fc0aade725f8d8d54d4ca9e676519c602f23bc14969d4162c7c1b35ad5388b660'
