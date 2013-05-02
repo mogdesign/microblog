853a6d9ffc2d642152c35a79280535de50c4251c
mino.lukac <mino.lukac@gmail.com>
2013-05-03 00:33:31 +0200

    Making the new list as front page, some styling of the output

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Microblog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
+  root :to => 'posts#list'
 
   # See how all your routes lay out with "rake routes"
 
